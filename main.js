29c02fc1328ac9b45d734eaf791061e7280363a6
clementpaul112 <72744246+clementpaul112@users.noreply.github.com>
2021-04-27 11:01:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song_1 = "";
 song_2 = "";
+left_wrist_x = 0;
+left_wrist_y = 0;
+right_wrist_x = 0;
+right_wrist_y = 0;
 
 function preload()
 {
@@ -13,6 +17,8 @@ function setup()
     canvas.center(); 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
     
 }
 
@@ -21,4 +27,21 @@ function draw()
     image(video,0,0,600,500);
 }
 
+function modelLoaded()
+{
+    console.log("Posenet is intialized");
+}
 
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        left_wrist_x = results[0].pose.leftWrist.x;
+        left_wrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWristx" + left_wrist_x + "left_wrist_y" + left_wrist_y);
+        right_wrist_x = results[0].pose.rightWrist.x;
+        right_wrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWristx" + right_wrist_x + "right_wrist_y" + right_wrist_y);
+    } 
+}
